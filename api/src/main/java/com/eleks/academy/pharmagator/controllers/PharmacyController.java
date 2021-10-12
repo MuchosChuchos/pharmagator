148651b034e36718dbc9604f271030947c0b0c09
TOlya <toly.artmchk@gmail.com>
2021-10-12 07:30:57 +0300

    Add CRUD methods for Pharmacy entity

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
@@ -3,10 +3,10 @@ package com.eleks.academy.pharmagator.controllers;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
 import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,10 +14,37 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/pharmacies")
 public class PharmacyController {
+
     private final PharmacyRepository pharmacyRepository;
 
     @GetMapping
     public ResponseEntity<List<Pharmacy>> getAll() {
-        return ResponseEntity.ok(pharmacyRepository.findAll());
+        List<Pharmacy> pharmacyList = pharmacyRepository.findAll();
+        return ResponseEntity.ok(pharmacyList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pharmacy> getById(@PathVariable("id") Long id) {
+        Pharmacy pharmacy = pharmacyRepository.findById(id).get();
+        return ResponseEntity.ok(pharmacy);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pharmacy> createPharmacy(@RequestBody Pharmacy pharmacy) {
+        return new ResponseEntity<>(pharmacyRepository.save(pharmacy), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Pharmacy> updatePharmacy(@PathVariable("id") Long id, @RequestBody Pharmacy pharmacy) {
+        pharmacy.setId(id);
+        return ResponseEntity.ok(pharmacyRepository.save(pharmacy));
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
+        Pharmacy pharmacy = pharmacyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid pharmacy id:" + id));
+        pharmacyRepository.delete(pharmacy);
+
+        return ResponseEntity.ok("Pharmacy was successfully deleted!");
     }
 }
