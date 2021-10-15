3feda8b3a187d41f64bcfa15f2a167a6128a8f41
TOlya <toly.artmchk@gmail.com>
2021-10-15 11:03:04 +0300

    Change V1_0_1 migration for passing failed test on travis

--- api/src/main/resources/db/migration/V1_0_1__create_relationships.sql
+++ api/src/main/resources/db/migration/V1_0_1__create_relationships.sql
@@ -1,3 +1,5 @@
 ALTER TABLE prices
-ADD CONSTRAINT fk_prices_medicines FOREIGN KEY (medicine_id) REFERENCES medicines (id),
+ADD CONSTRAINT fk_prices_medicines FOREIGN KEY (medicine_id) REFERENCES medicines (id);
+
+ALTER TABLE prices
 ADD CONSTRAINT fk_prices_pharmacies FOREIGN KEY (pharmacy_id) REFERENCES pharmacies (id);
