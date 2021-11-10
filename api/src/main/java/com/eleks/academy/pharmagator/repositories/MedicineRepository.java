e91e298c73de4621d6dc5f99cf1107c70d87b2cc
TOlya <toly.artmchk@gmail.com>
2021-11-10 15:45:55 +0200

    feat: Store dto to db
    Add method storeToDatabase implementation.
    Add method to repository for
    more proper saving dto.
    Change modelmapper config to disable mapping
    wrong fields

--- api/src/main/java/com/eleks/academy/pharmagator/repositories/MedicineRepository.java
+++ api/src/main/java/com/eleks/academy/pharmagator/repositories/MedicineRepository.java
@@ -4,4 +4,7 @@ import com.eleks.academy.pharmagator.entities.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+
+    Medicine findByTitle(String title);
+
 }
--- api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
+++ api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
@@ -2,8 +2,15 @@ package com.eleks.academy.pharmagator.scheduler;
 
 import com.eleks.academy.pharmagator.dataproviders.DataProvider;
 import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
+import com.eleks.academy.pharmagator.entities.Medicine;
+import com.eleks.academy.pharmagator.entities.Pharmacy;
+import com.eleks.academy.pharmagator.entities.Price;
+import com.eleks.academy.pharmagator.repositories.MedicineRepository;
+import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
+import com.eleks.academy.pharmagator.repositories.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,14 +26,48 @@ import java.util.concurrent.TimeUnit;
 public class Scheduler {
 
     private final List<DataProvider> dataProviderList;
+    private final PriceRepository priceRepository;
+    private final PharmacyRepository pharmacyRepository;
+    private final MedicineRepository medicineRepository;
+    private final ModelMapper modelMapper;
 
-    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(fixedDelay = 100, timeUnit = TimeUnit.MINUTES)
     public void schedule() {
         log.info("Scheduler started at {}", Instant.now());
-        dataProviderList.stream().flatMap(DataProvider::loadData).forEach(this::storeToDatabase);
+        dataProviderList.parallelStream()
+                .flatMap(DataProvider::loadData)
+                .forEach(this::storeToDatabase);
+        log.info("Scheduler finished at {}", Instant.now());
     }
 
     private void storeToDatabase(MedicineDto dto) {
-        log.info(dto.getTitle() + " - " + dto.getPrice());
+        Medicine medicine = modelMapper.map(dto, Medicine.class);
+        Price price = modelMapper.map(dto, Price.class);
+
+
+        String pharmacyName = dto.getPharmacyName();
+        Pharmacy pharmacyFromDb = pharmacyRepository.findByName(pharmacyName);
+        if (pharmacyFromDb == null) {
+            Pharmacy pharmacy = new Pharmacy();
+            pharmacy.setName(pharmacyName);
+            pharmacyFromDb = pharmacyRepository.save(pharmacy);
+        }
+
+
+        Long medicineId;
+        Medicine medFromDb = medicineRepository.findByTitle(medicine.getTitle());
+        if (medFromDb != null) {
+            medicineId = medFromDb.getId();
+        } else {
+            medicineId = medicineRepository.save(medicine).getId();
+        }
+
+
+        price.setMedicineId(medicineId);
+        price.setPharmacyId(pharmacyFromDb.getId());
+        price.setUpdatedAt(Instant.now());
+        price.setExternalId(dto.getExternalId());
+        priceRepository.save(price);
     }
+
 }
