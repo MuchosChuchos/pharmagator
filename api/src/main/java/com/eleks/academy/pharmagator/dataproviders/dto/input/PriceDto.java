d20afe65ae7dbaffa068144944defba085937c01
TOlya <toly.artmchk@gmail.com>
2021-11-15 11:28:46 +0200

    refactor: Set @NotNull for price field.

--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/PriceDto.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/PriceDto.java
@@ -4,13 +4,14 @@ import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class PriceDto {
 
     @Min(value = 0)
-    @NotBlank
+    @NotNull
     private BigDecimal price;
 
     @NotBlank
