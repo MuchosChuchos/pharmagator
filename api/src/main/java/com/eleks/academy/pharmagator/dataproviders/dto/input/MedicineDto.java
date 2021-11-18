2781e9d923eade053d612d1a04684ca16e3134eb
TOlya <toly.artmchk@gmail.com>
2021-11-18 08:16:55 +0200

    refactor(Add @NoArgs... and @AllArgs... to dtos):

--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/MedicineDto.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/MedicineDto.java
@@ -1,10 +1,14 @@
 package com.eleks.academy.pharmagator.dataproviders.dto.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MedicineDto {
 
     @NotBlank
--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/PriceDto.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/input/PriceDto.java
@@ -1,6 +1,8 @@
 package com.eleks.academy.pharmagator.dataproviders.dto.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -8,6 +10,8 @@ import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PriceDto {
 
     @Min(value = 0)
