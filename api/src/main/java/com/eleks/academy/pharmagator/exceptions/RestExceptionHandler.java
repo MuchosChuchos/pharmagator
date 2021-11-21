9470ae88bfd1c91ecffcee7a1c68bb2e07daeb9f
TOlya <toly.artmchk@gmail.com>
2021-11-21 15:11:38 +0200

    refactor: change rest controller exception handling
    Extend scope of class hierarchy for response entity.
    Change annotation.
    Shrink methods body.

--- api/src/main/java/com/eleks/academy/pharmagator/exceptions/RestExceptionHandler.java
+++ api/src/main/java/com/eleks/academy/pharmagator/exceptions/RestExceptionHandler.java
@@ -2,24 +2,22 @@ package com.eleks.academy.pharmagator.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
-    protected ResponseEntity<Object> handleException(ResponseStatusException ex, WebRequest request) {
-        ApiError apiError = new ApiError(ex.getReason());
-        return new ResponseEntity<>(apiError, ex.getStatus());
+    protected ResponseEntity<ApiError> handleException(ResponseStatusException ex, WebRequest request) {
+        return new ResponseEntity<>(new ApiError(ex.getReason()), ex.getStatus());
     }
 
     @ExceptionHandler(InvalidIdentifierException.class)
-    protected ResponseEntity<Object> handleException(InvalidIdentifierException ex, WebRequest request) {
-        ApiError apiError = new ApiError(ex.getMessage());
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    protected ResponseEntity<ApiError> handleException(InvalidIdentifierException ex, WebRequest request) {
+        return new ResponseEntity<>(new ApiError(ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
 }
